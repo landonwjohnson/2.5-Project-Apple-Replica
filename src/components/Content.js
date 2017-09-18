891339628e468759ec5fd2592b9a270c091e4d4c
Landon <landonwjohnson@gmail.com>
2017-09-18 12:48:33 -0600

    worked on homepage

--- src/components/Content.js
+++ src/components/Content.js
@@ -4,10 +4,16 @@ import ReactDOM from 'react-dom';
 export default class Content extends React.Component {
     render() {
         return (
-            <div className="content1">
-                <div>
-                    <h1> iPhone 8 </h1>
-                    <h2> A new generation of iPhone </h2>
+            <div>
+                <div className="content1">
+                    <h2> iPhone 8 </h2>
+                    <h3> A new generation of iPhone </h3>
+                    <img/>
+                </div>
+                
+                <div className="content2">
+                    <h2> iPhone X </h2>
+                    <h3> Say hello to the future </h3>
                     <img/>
                 </div>
               
