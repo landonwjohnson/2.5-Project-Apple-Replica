97bedde0ecaea35c05a1b2011a4fa90e11e50b5c
landonwjohnson <landonwjohnson@gmail.com>
2017-09-26 10:33:31 -0600

    made the cart have some functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AppleIcon from '../img/svg/Apple-Icon.svg';
 import SearchIcon from '../img/svg/Search-Icon.svg';
-import CartIcon from '../img/svg/Cart-Icon.svg'
+import CartIcon from '../img/svg/Cart-Icon.svg';
+import classnames from 'classnames';
 
 export default class Header extends React.Component {
 
-    
+
+    constructor(){
+        super();
+        this.state ={
+            cartMenuOpen: false
+        }
+
+        this.handleCartClick = this.handleCartClick.bind(this);
+    }
+    handleCartClick(){
+        console.log('Hi');
+        if(this.state.cartMenuOpen){
+            this.setState({cartMenuOpen: false})
+        }
+        else {
+            this.setState({cartMenuOpen: true})
+        }
+    }
     render() {
+        var bagClasses = classnames({
+            "bagview-content--show": this.state.cartMenuOpen,
+            "bagview-content": true
+        })
         return (
             <div>
                 <div className="ac-gn-header">
@@ -21,15 +43,15 @@ export default class Header extends React.Component {
                         <li>Music</li>
                         <li>Support</li>
                         <li className="searchIcon" ><img src={SearchIcon} /> </li>
-                        <li className="cartIcon"><img src={CartIcon} /> </li>
+                        <li className="cartIcon" onClick={this.handleCartClick} ><img src={CartIcon} /> </li>
                     </ul>
                     <ul className="mobile-nav">
                         <li className ="menuicon-bread"> <span className = "menuicon-bread-crust-top"/> <span className = "menuicon-bread-crust-bottom"/> </li>
                         <li className="appleIcon"><img src={AppleIcon}/></li>
-                        <li className="cartIcon"><img src={CartIcon} /> </li>
+                        <li className="cartIcon" ><img src={CartIcon} /></li>
                     </ul>
                 </div>
-                <div id="bagview-content">
+                <div className={bagClasses}>
                                 <p> Your Bag is empty. </p>
                                     <ul>
                                         <li> Bag </li>
