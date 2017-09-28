a856d9f6c00b5af463ea0c9d1bb6bdb4256e16cb
Landon <landonwjohnson@gmail.com>
2017-09-28 13:05:00 -0600

    updated cart menu, the mobile nav bar, and is responsive on screen size w320

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ import CartIcon from '../img/svg/Cart-Icon.svg';
 import BagIcon from '../img/svg/cart-menu/bag-icon.svg';
 import FavIcon from '../img/svg/cart-menu/favorites-icon.svg';
 import OrdersIcon from '../img/svg/cart-menu/orders-icon.svg';
-import AccountIcon from '../img/svg/cart-menu/orders-icon.svg';
+import AccountIcon from '../img/svg/cart-menu/account-icon.svg';
 import SignInIcon from '../img/svg/cart-menu/sign-in-icon.svg';
 
 
@@ -17,13 +17,15 @@ export default class Header extends React.Component {
     constructor(){
         super();
         this.state ={
-            cartMenuOpen: false
+            cartMenuOpen: false,
+            mobileNavOpen: false
         }
-
         this.handleCartClick = this.handleCartClick.bind(this);
+        this.handleMobileNavClick = this.handleMobileNavClick.bind(this);
     }
     handleCartClick(){
         console.log('Hi');
+        this.setState({mobileNavOpen: false})
         if(this.state.cartMenuOpen){
             this.setState({cartMenuOpen: false})
         }
@@ -31,16 +33,32 @@ export default class Header extends React.Component {
             this.setState({cartMenuOpen: true})
         }
     }
+
+    handleMobileNavClick(){
+        console.log('mobile click test')
+        this.setState({cartMenuOpen: false})
+        if(this.state.mobileNavOpen){
+            this.setState({mobileNavOpen: false})
+        }
+        else {
+            this.setState({mobileNavOpen: true})
+        }
+    }
     render() {
         var bagClasses = classnames({
             "bagview-content--show": this.state.cartMenuOpen,
             "bagview-content": true
         })
+
+        var mobileNavClasses = classnames({
+            "mobile-nav--show": this.state.mobileNavOpen,
+            "mobile-nav": true
+        })
         return (
             <div>
                 <div className="ac-gn-header">
                     <ul className="nav">
-                        <li className ="menuicon-bread"> <span className = "menuicon-bread-crust-top"/> <span className = "menuicon-bread-crust-bottom"/> </li>
+                        <li className ="menuicon-bread" onClick={this.handleMobileNavClick}> <span className = "menuicon-bread-crust-top"/> <span className = "menuicon-bread-crust-bottom"/> </li>
                         <li className="appleIcon"><img src={AppleIcon}/></li>
                         <li>Mac</li>
                         <li>iPad</li>
@@ -68,15 +86,17 @@ export default class Header extends React.Component {
                 </div>
                
                 </div>
-                <ul className="mobile-nav">
-                        <li>Mac</li>
-                        <li>iPad</li>
-                        <li>iPhone</li>
-                        <li>Watch</li>
-                        <li>TV</li>
-                        <li>Music</li>
-                        <li>Support</li>
+                <div className={mobileNavClasses}>
+                    <ul>
+                        <li className="m-nav-list">Mac</li>
+                        <li className="m-nav-list">iPad</li>
+                        <li className="m-nav-list">iPhone</li>
+                        <li className="m-nav-list">Watch</li>
+                        <li className="m-nav-list">TV</li>
+                        <li className="m-nav-list">Music</li>
+                        <li className="m-nav-list">Support</li>
                     </ul>  
+                </div>
                     
             </div>
         );
