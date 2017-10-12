935885d844325effef04b337ffb8b7a2976a87f2
Landon <landonwjohnson@gmail.com>
2017-10-12 13:03:13 -0600

    added HashRouter

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,18 +4,20 @@ import Content1 from './content/Content1';
 import Content2 from './content/Content2';
 import Content4 from './content/Content4';
 import Content7 from './content/Content7';
-import {Route, Switch} from 'react-router-dom';
+import {Route, Switch, HashRouter} from 'react-router-dom';
 
 export default class Body extends React.Component {
     render() {
         return(
             <div>
+                <HashRouter>
                 <Switch>
                     <Route component={ Content2 } path="/Mac" />
                     <Route component={ Content4 } path="/iPhone"/>
                     <Route component={ Content7 } path="/Music" />
                     <Route component={ Content1 } path="/" />
                 </Switch>
+                </HashRouter>
             </div>
         )
     }
